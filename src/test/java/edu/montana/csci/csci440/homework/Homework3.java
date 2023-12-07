e1a68d6c8ea9f0bdfd79b681390bd82098ed9e8c
jamesonmac4 <jameson.macmillan@warriorlife.net>
2023-12-06 23:50:01 -0700

    Homework 3

--- src/test/java/edu/montana/csci/csci440/homework/Homework3.java
+++ src/test/java/edu/montana/csci/csci440/homework/Homework3.java
@@ -33,16 +33,15 @@ public class Homework3 extends DBTest {
 
         try(Connection connection = DB.connect()){
             connection.setAutoCommit(false);
-            PreparedStatement subtract = connection.prepareStatement("TODO");
-            subtract.setLong(1, 0);
-            subtract.setLong(2, 0);
+            PreparedStatement subtract = connection.prepareStatement("UPDATE tracks SET Milliseconds = Milliseconds - 10 WHERE TrackId = ?");
+            subtract.setLong(1, 1);
             subtract.execute();
 
-            PreparedStatement add = connection.prepareStatement("TODO");
-            add.setLong(1, 0);
-            add.setLong(2, 0);
+            PreparedStatement add = connection.prepareStatement("UPDATE tracks SET Milliseconds = Milliseconds + 10 WHERE TrackId = ?");
+            add.setLong(1, 2);
             add.execute();
 
+            connection.commit();
             // commit with the connection
         }
 
@@ -57,21 +56,34 @@ public class Homework3 extends DBTest {
     /*
      * Select tracks that have been sold more than once (> 1)
      *
-     * Select the albumbs that have tracks that have been sold more than once (> 1)
+     * Select the albums that have tracks that have been sold more than once (> 1)
      *   NOTE: This is NOT the same as albums whose tracks have been sold more than once!
      *         An album could have had three tracks, each sold once, and should not be included
-     *         in this result.  It should only include the albums of the tracks found in the first
+     *         in this result. It should only include the albums of the tracks found in the first
      *         query.
      * */
     public void selectPopularTracksAndTheirAlbums() throws SQLException {
 
         // HINT: join to invoice items and do a group by/having to get the right answer
-        List<Map<String, Object>> tracks = executeSQL("");
+        List<Map<String, Object>> tracks = executeSQL("SELECT *, SUM(Quantity) as quan\n" +
+                "FROM tracks\n" +
+                "JOIN invoice_items on tracks.TrackId = invoice_items.TrackId\n" +
+                "GROUP BY invoice_items.TrackId\n" +
+                "HAVING quan > 1;");
         assertEquals(256, tracks.size());
 
         // HINT: join to tracks and invoice items and do a group by/having to get the right answer
         //       note: you will need to use the DISTINCT operator to get the right result!
-        List<Map<String, Object>> albums = executeSQL("");
+        List<Map<String, Object>> albums = executeSQL(
+                "SELECT DISTINCT albums.AlbumId, albums.Title\n" +
+                        "FROM albums\n" +
+                        "         JOIN tracks ON albums.AlbumId = tracks.AlbumId\n" +
+                        "WHERE tracks.TrackId IN (\n" +
+                        "    SELECT tracks.TrackId\n" +
+                        "    FROM tracks\n" +
+                        "             JOIN invoice_items ON tracks.TrackId = invoice_items.TrackId\n" +
+                        "    GROUP BY tracks.TrackId\n" +
+                        "    HAVING COUNT(invoice_items.InvoiceLineId) > 1)");
         assertEquals(166, albums.size());
     }
 
@@ -84,7 +96,22 @@ public class Homework3 extends DBTest {
      * */
     public void selectCustomersMeetingCriteria() throws SQLException {
         // HINT: join to invoice items and do a group by/having to get the right answer
-        List<Map<String, Object>> tracks = executeSQL("" );
+        List<Map<String, Object>> tracks = executeSQL("SELECT customers.Email\n" +
+                "FROM customers\n" +
+                "WHERE customers.SupportRepId IN (\n" +
+                "    SELECT employees.EmployeeId\n" +
+                "    FROM employees\n" +
+                "    WHERE employees.FirstName = 'Jane' AND employees.LastName = 'Peacock'\n" +
+                ")\n" +
+                "  AND customers.CustomerId IN (\n" +
+                "    SELECT invoices.CustomerId\n" +
+                "    FROM invoices\n" +
+                "             JOIN invoice_items ON invoices.InvoiceId = invoice_items.InvoiceId\n" +
+                "             JOIN tracks ON invoice_items.TrackId = tracks.TrackId\n" +
+                "             JOIN genres  ON tracks.GenreId = genres.GenreId\n" +
+                "    WHERE genres.Name = 'Rock'\n" +
+                "    GROUP BY invoices.CustomerId\n" +
+                "    HAVING COUNT(invoice_items.InvoiceLineId) > 0)" );
         assertEquals(21, tracks.size());
     }
 
